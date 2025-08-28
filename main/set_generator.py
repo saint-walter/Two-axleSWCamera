ccb069003c98b60f4d98eadca1f18c1743f75929
saint-walter <mrvladiks080@gmail.com>
2025-08-28 19:48:49 +0300

    Update set_generator.py

--- main/set_generator.py
+++ main/set_generator.py
@@ -2,7 +2,6 @@ import cv2
 import os
 import sys
 
-# Получение ID из аргументов командной строки
 if len(sys.argv) < 2:
     print("Usage: python set_generator.py <id>")
     sys.exit(1)
@@ -25,7 +24,6 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
-        # Сохраняем изображения в папке dataset
         cv2.imwrite(f"dataset/{face_id}.{count}.jpg", roi_gray)
         count += 1
         print(f"Set {count} images")
@@ -33,8 +31,8 @@ while True:
     cv2.imshow("img", img)
     k = cv2.waitKey(10)
 
-    if k == 27:  # Нажатие клавиши Esc для выхода
+    if k == 27:
         break
-    elif count >= 100:  # Завершаем, если собрано 100 изображений
+    elif count >= 100:  
         print("Done!")
         break
